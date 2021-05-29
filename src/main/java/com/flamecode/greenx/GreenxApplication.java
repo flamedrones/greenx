da8bc9dd7933bd1ae7e39b054c2560b8374fd463
sweethuman <gheo@sweethuman.tech>
2021-05-29 11:16:33 +0300

    init repo

---
+++ src/main/java/com/flamecode/greenx/GreenxApplication.java
@@ -0,0 +1,13 @@
+package com.flamecode.greenx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flamecode/greenx/GreenxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flamecode.greenx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
